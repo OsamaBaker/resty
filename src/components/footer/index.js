27d290b3acee3775d786e0b2d9fe53f18c0de94d
Osama Baker <edewanjy@hotmail.com>
2021-09-25 19:06:39 +0300

    class29

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
+
+
+
+import React from 'react'
 
 function Footer() {
-
-    return (
-      <div>
-      <footer>&copy; 2018</footer>;
-      </div>
-    ) 
-  
+  return (
+    <div>
+      <footer>&copy; 2018</footer>
+    </div>
+  )
 }
 
-export default Footer;
+export default Footer
